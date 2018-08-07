1f4eacbca5d01d648d847c59b31315d89e692a5c
hallm13 <hallm0013@gmail.com>
2018-08-07 14:19:44 -0500

    add next and previous functions

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -1,6 +1,6 @@
 {
   $('button#play-pause').on('click', function() {
-    player.playPause();
+    helper.playPauseAndUpdate();
     $(this).attr('playState', player.playState);
   });
 
@@ -13,7 +13,7 @@
     if (nextSongIndex >= album.songs.length) { return; }
 
     const nextSong = album.songs[nextSongIndex];
-    player.playPause(nextSong);
+    helper.playPauseAndUpdate(nextSong);
   });
 
   // Previous Track button
@@ -25,6 +25,24 @@
     if (previousSongIndex >= album.songs.length) { return; }
 
     const previousSong = album.songs[previousSongIndex];
-    player.playPause(previousSong);
+    helper.playPauseAndUpdate(previousSong);
   });
+
+  $('#time-control input').on('input', function (event) {
+       player.skipTo(event.target.value);
+  });
+
+  setInterval( () => {
+     if (player.playState !== 'playing') { return; }
+     const currentTime = player.getTime();
+     const duration = player.getDuration();
+     const percent = (currentTime / duration) * 100;
+     $('#time-control .current-time').text( currentTime );
+     $('#time-control input').val(percent);
+   }, 1000);
+
+   $('#volume-control input').on('input', function(event) {
+     player.setVolume(event.target.value);
+   });
+
 }
